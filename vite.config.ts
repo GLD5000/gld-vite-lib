ac9dead91a8659a656dbf8ab8018c4dc8ee225d8
GLD5000 <garethlouisdevlin@gmail.com>
2025-07-17 22:41:01 +0100

    Update tsconfig naming

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] ,tsconfigPath: './tsconfig-build.json'})],
+  plugins: [react(), dts({ include: ["lib"] ,tsconfigPath: './tsconfig.lib.json'})],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
